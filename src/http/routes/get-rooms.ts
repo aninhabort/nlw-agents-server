d51ea144b4d4e0f19c16a1a4f2dbb519c03fdc3e
aninhabort <carol2015bortolini@gmail.com>
2025-07-09 17:23:12 +0100

    feat: add create question, create rooms and get room questions

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { schema } from "../../db/schema/index.ts";
 import { db } from "../../db/connection.ts";
-import { count } from "drizzle-orm";
+import { count, eq } from "drizzle-orm";
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get("/rooms", async () => {
@@ -9,20 +9,13 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        createdAt: schema.rooms.createdAt,
+        questionsCount: count(schema.questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.createdAt);
-    // const results = await db
-    //   .select({
-    //     id: schema.rooms.id,
-    //     name: schema.rooms.name,
-    //     createdAt: schema.rooms.createdAt,
-    //     questionsCount: count(schema.questions.id),
-    //   })
-    //   .from(schema.rooms)
-    //   .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
-    //   .groupBy(schema.rooms.id)
-    //   .orderBy(schema.rooms.createdAt);
 
     return results;
   });
